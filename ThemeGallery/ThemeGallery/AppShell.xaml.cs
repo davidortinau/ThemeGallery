487c66f63c9c0f7de2290732bc9e991f8a988b07
David Ortinau <david.ortinau@microsoft.com>
2019-12-26 10:39:07 -0600

    added menu items for changing themes

--- ThemeGallery/ThemeGallery/AppShell.xaml.cs
+++ ThemeGallery/ThemeGallery/AppShell.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ThemeGallery.Styles;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,9 +11,31 @@ namespace ThemeGallery
 {
     public partial class AppShell
     {
+        public Command ChangeThemeCommand { get; set; }
+
         public AppShell()
         {
+            BindingContext = this;
+
+            ChangeThemeCommand = new Command<string>(OnChangeTheme);
+
             InitializeComponent();
+        }
+
+        private void OnChangeTheme(string theme)
+        {
+            if (theme == "dark")
+            {
+                App.Current.Resources = new DarkTheme();
+                App.AppTheme = theme;
+            }
+            else
+            {
+                App.Current.Resources = new LightTheme();
+                App.AppTheme = theme;
+            }
+
+            Shell.Current.FlyoutIsPresented = false;
         }
     }
 }
