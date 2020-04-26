b2237ac338b75ec2ee554b182fbcd7fd7e49f6db
David Ortinau <david.ortinau@microsoft.com>
2020-04-25 20:06:21 -0500

    fluent entry

---
+++ ThemeGallery/ThemeGallery/Views/EntriesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ThemeGallery.Views
+{
+    public partial class EntriesPage : ContentPage
+    {
+        public EntriesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
