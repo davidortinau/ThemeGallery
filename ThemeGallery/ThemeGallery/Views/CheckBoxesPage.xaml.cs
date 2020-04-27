283389e44ebe7ca872fb46660bdc724a71e07d65
David Ortinau <david.ortinau@microsoft.com>
2020-04-26 21:39:55 -0500

    switch, radio, and check

---
+++ ThemeGallery/ThemeGallery/Views/CheckBoxesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ThemeGallery.Views
+{
+    public partial class CheckBoxesPage : ContentPage
+    {
+        public CheckBoxesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ThemeGallery/ThemeGallery/Views/RadioButtonsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ThemeGallery.Views
+{
+    public partial class RadioButtonsPage : ContentPage
+    {
+        public RadioButtonsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
